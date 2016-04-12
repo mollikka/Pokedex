01d2d0482aa7483cf0a0bbe0d3856d07d7850361
Lauri Tervonen <lauri.tervonen@aalto.fi>
2016-04-12 11:47:48 +0300

    render search results on the page

--- client/pokemonsearch.js
+++ client/pokemonsearch.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
+  var template;
+
+  $.ajax({
+    url: "pokeresult.html",
+    success: function(html) {template = Handlebars.compile(html);},
+  });
+
   $('form').on('submit',function(e){
     e.preventDefault();
     $.ajax({
       type: 'GET',
       url: "pokemon/"+$('#pokesearch').val(),
-      success: function(response) {alert(response.name);},
+      success: function(response) {add_result(response);},
       error: function() {alert("Pokemon not found");},
     });
   });
+
+  function add_result(json){
+    $('#pokeresults').prepend(template(json));
+  }
 });
