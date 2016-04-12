a7411a73543e68a35fdcf10c21ce60eb3075cbbb
Lauri Tervonen <lauri.tervonen@aalto.fi>
2016-04-12 12:45:50 +0300

    make frontend great again

--- client/pokemonsearch.js
+++ client/pokemonsearch.js
@@ -6,6 +6,16 @@ $(document).ready(function(){
     success: function(html) {template = Handlebars.compile(html);},
   });
 
+  Handlebars.registerHelper('list', function(items, options) {
+    var out = "<ul>";
+
+    for(var i=0, l=items.length; i<l; i++) {
+      out = out + "<li>" + options.fn(items[i]) + "</li>";
+    }
+
+    return out + "</ul>";
+  });
+
   $('form').on('submit',function(e){
     e.preventDefault();
     $.ajax({
